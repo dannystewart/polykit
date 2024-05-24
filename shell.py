17fa57bfb6ff119340a368181d776b16d60bc266
Danny Stewart <danny@stewart.cc>
2024-05-24 19:09:53 -0400

    dsutil.shell (refactor): Import ColorName from dsutil.text

--- shell.py
+++ shell.py
@@ -8,29 +8,9 @@ import os
 import subprocess
 import sys
 from functools import wraps
-from typing import Callable, Literal
+from typing import Callable
 
-from dsutil.text import color
-
-ColorName = Literal[
-    "black",
-    "grey",
-    "red",
-    "green",
-    "yellow",
-    "blue",
-    "magenta",
-    "cyan",
-    "light_grey",
-    "dark_grey",
-    "light_red",
-    "light_green",
-    "light_yellow",
-    "light_blue",
-    "light_magenta",
-    "light_cyan",
-    "white",
-]
+from dsutil.text import ColorName, color
 
 
 def handle_keyboard_interrupt(
