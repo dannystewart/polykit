c3b7cf96590cc70d69e1a3ad505e10ade0149076
Danny Stewart <danny@stewart.cc>
2024-05-31 14:19:43 -0400

    dsutil.macos (docs): Add module docstring and type hints

--- macos.py
+++ macos.py
@@ -1,30 +1,34 @@
+"""
+This module contains macOS-specific functions and utilities.
+"""
+
 import subprocess
 
 
-def get_timestamps(file):
+def get_timestamps(file: str) -> tuple[str, str]:
     """
     Gets file creation and modification timestamps. macOS only, as it relies on GetFileInfo.
 
     Args:
-        file (str): The file to get the timestamps of.
+        file: The file to get the timestamps of.
 
     Returns:
-        ctime (str): The creation timestamp.
-        mtime (str): The modification timestamp.
+        ctime: The creation timestamp.
+        mtime: The modification timestamp.
     """
     ctime = subprocess.check_output(["GetFileInfo", "-d", file]).decode().strip()
     mtime = subprocess.check_output(["GetFileInfo", "-m", file]).decode().strip()
     return ctime, mtime
 
 
-def set_timestamps(file, ctime=None, mtime=None):
+def set_timestamps(file: str, ctime: str | None = None, mtime: str | None = None) -> None:
     """
     Sets file creation and/or modification timestamps. macOS only, as it relies on SetFile.
 
     Args:
-        file (str): The file to set the timestamps of.
-        ctime (str, optional): The creation timestamp to set. If None, creation time won't be set.
-        mtime (str, optional): The modification timestamp to set. If None, modification time won't be set.
+        file: The file to set the timestamps of.
+        ctime: The creation timestamp to set. If None, creation time won't be set.
+        mtime: The modification timestamp to set. If None, modification time won't be set.
     """
     if ctime is None and mtime is None:
         raise ValueError("At least one of ctime or mtime must be set.")
