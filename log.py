9c363eb1072f2dc0c9e10937b4e8afcabf5160b6
Danny Stewart <danny@stewart.cc>
2024-06-09 14:09:02 -0400

    LocalLogger (enhancement): Streamline logger setup and docstrings

--- log.py
+++ log.py
@@ -1,7 +1,7 @@
 """
-This module provides classes for setting up loggers and formatting them. The LocalLogger class
-provides a method for setting up a logger with a console handler. The ConsoleColors class defines
-console color codes for use in the formatter to colorize messages by log level.
+Classes for setting up and formatting loggers. The LocalLogger class provides a method for setting
+up a logger with a console handler. The ConsoleColors class defines console color codes for use in
+the formatter to colorize messages by log level.
 """
 
 import logging
@@ -16,13 +16,14 @@ FormatterLevel = Literal["basic", "advanced"]
 
 class LocalLogger:
     """
-    This class is used to set up an easy local logger with a console handler. The level is set to
-    INFO by default, but can be set to any level using the log_level parameter. The logger uses a
-    custom formatter that includes the time, logger name, function name, and log message.
+    Set up an easy local logger with a console handler. The level is set to INFO by default, but can
+    be set to any level using the log_level parameter. The logger uses a custom formatter that
+    includes the time, logger name, function name, and log message.
 
     Usage:
         from dsutil.log import LocalLogger
-        logger = LocalLogger.setup_logger("my_logger")
+        logger = LocalLogger.setup_logger(self.__class__.__name__)
+        logger = LocalLogger.setup_logger("MyClassLogger", advanced=True)
     """
 
     LEVEL_COLORS = {
@@ -38,14 +39,12 @@ class LocalLogger:
     GRAY = "\033[90m"
 
     @staticmethod
-    def setup_logger(
-        logger_name: str, level: int = logging.INFO, formatter: FormatterLevel = "basic"
-    ) -> logging.Logger:
+    def setup_logger(logger_name: str, level: int = logging.INFO, basic: bool = False) -> logging.Logger:
         """Set up a logger with the given name and log level."""
         logger = logging.getLogger(logger_name)
         logger.setLevel(level)
 
-        log_formatter = LocalLogger.CustomFormatter(formatter)
+        log_formatter = LocalLogger.CustomFormatter(basic=basic)
 
         console_handler = logging.StreamHandler()
         console_handler.setFormatter(log_formatter)
@@ -58,17 +57,18 @@ class LocalLogger:
     class CustomFormatter(logging.Formatter):
         """Custom log formatter supporting both basic and advanced formats."""
 
-        def __init__(self, style: FormatterLevel) -> None:
+        def __init__(self, basic: bool = False) -> None:
             super().__init__()
-            self.style = style
+            self.basic = basic
 
-        def formatTime(self, record: logging.LogRecord, datefmt: str | None = None) -> str:
+        def formatTime(self, record: logging.LogRecord, datefmt: str | None = None) -> str:  # noqa
             """Format the time in a log record."""
             tz = ZoneInfo(os.getenv("TZ", "America/New_York"))
             ct = datetime.fromtimestamp(record.created, tz=tz)
             return ct.strftime(datefmt) if datefmt else ct.isoformat()
 
         def format(self, record: logging.LogRecord) -> str:
+            """Format the log record based on the formatter style."""
             level_color = LocalLogger.LEVEL_COLORS.get(record.levelname, LocalLogger.RESET)
             reset = LocalLogger.RESET
             bold = LocalLogger.BOLD
@@ -76,11 +76,11 @@ class LocalLogger:
 
             record.asctime = self.formatTime(record, "%I:%M:%S %p")
 
-            if self.style == "basic":
+            if self.basic:
                 return f"{reset}{bold}{level_color}{record.getMessage()}{reset}"
 
             timestamp = f"{reset}{gray}{record.asctime}{reset}"
             msg_color = (
                 f"{reset}{bold}{level_color}" if record.levelname not in ["DEBUG", "INFO"] else f"{reset}"
             )
-            return f"{timestamp} {level_color}{record.levelname}: {msg_color}{record.getMessage()}{reset}"
+            return f"{timestamp} {level_color}[{record.levelname}] {msg_color}{record.getMessage()}{reset}"
