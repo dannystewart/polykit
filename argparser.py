a4e1842dca4f55643166fa870453a978b8c83f47
Danny Stewart <danny@stewart.cc>
2024-09-08 10:34:57 -0400

    ArgParser (docs): Expand docstrings for argument methods

--- argparser.py
+++ argparser.py
@@ -29,7 +29,12 @@ class CustomHelpFormatter(argparse.HelpFormatter):
     and help text. You would use this class by passing it as the formatter_class argument to the
     ArgumentParser constructor, like the below example.
 
-        parser = argparse.ArgumentParser(description=__doc__, formatter_class=lambda prog: CustomHelpFormatter(prog, max_help_position=24, width=100))
+        parser = argparse.ArgumentParser(
+            description=__doc__,
+            formatter_class=lambda prog: CustomHelpFormatter(
+                prog, max_help_position=24, width=100
+            ),
+        )
 
     But to make your life easier, just use the ArgParser class below, which uses this formatter by
     default and allows you to specify the column widths as arguments.
@@ -61,6 +66,10 @@ class ArgParser(argparse.ArgumentParser):
 
     The arg_width and max_width arguments are optional and default to 24 and 120, respectively. They
     map to max_help_position and width in CustomHelpFormatter.
+
+    This class also provides methods for defining and adding arguments in a more structured way:
+        - add_argument_from_info: Adds a single argument based on an ArgInfo instance.
+        - add_args_from_class: Adds multiple arguments based on a class of ArgInfo instances.
     """
 
     def __init__(self, *args, **kwargs):
@@ -77,7 +86,25 @@ class ArgParser(argparse.ArgumentParser):
         )
 
     def add_argument_from_info(self, name: str, arg_info: ArgInfo) -> None:
-        """Add an argument to the parser based on ArgInfo."""
+        """
+        Add an argument to the parser based on ArgInfo.
+
+        This method simplifies the process of adding arguments by using an ArgInfo instance, which
+        encapsulates all the necessary information for an argument. It automatically sets up the
+        appropriate argparse parameters based on the ArgInfo properties.
+
+        Args:
+            name: The name of the argument. For positional arguments, this is used as is. For
+                optional arguments, it's converted to the appropriate format (e.g. --name).
+            arg_info: An instance of ArgInfo containing the argument's properties.
+
+        The method handles different cases:
+            - For 'file' argument, it's added as a positional argument.
+            - For 'creation' and 'modification', both short (-c, -m) and long (--creation,
+                --modification) forms are added.
+            - For other arguments, they're added as long-form optional arguments (e.g.
+                --argument-name).
+        """
         kwargs: dict[str, Any] = {"help": arg_info.help}
 
         if arg_info.action in ["store_true", "store_false"]:
@@ -105,6 +132,24 @@ class ArgParser(argparse.ArgumentParser):
             self.add_argument(f"--{name.replace('_', '-')}", **kwargs)
 
     def add_args_from_class(self, arg_class: Type[ArgumentsBase]) -> None:
-        """Automatically add arguments to the parser based on a class of ArgInfo instances."""
+        """
+        Automatically add arguments to the parser based on a class of ArgInfo instances.
+
+        This method simplifies the process of adding multiple arguments by using a class that
+        contains ArgInfo instances as class attributes. It iterates through all ArgInfo instances in
+        the provided class and adds them to the parser using `add_argument_from_info`.
+
+        Args:
+            arg_class: A class that inherits from ArgumentsBase and contains ArgInfo instances as
+                class attributes.
+
+        Example usage:
+            class MyArguments(ArgumentsBase):
+                file = ArgInfo(help="Input file path", type=str, required=True)
+                verbose = ArgInfo(help="Increase output verbosity", action="store_true")
+
+            parser = ArgParser(description="My program description")
+            parser.add_args_from_class(MyArguments)
+        """
         for field_name, arg_info in arg_class.as_dict().items():
             self.add_argument_from_info(field_name, arg_info)
