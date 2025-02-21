af288828f0a8422254e498d7e5ff578d8a96e04c
Danny Stewart <danny@stewart.cc>
2025-02-20 21:53:28 -0500

    MediaManager (feature): Add MediaManager and VideoHelper classes for media operations

--- src/dsutil/__init__.py
+++ src/dsutil/__init__.py
@@ -3,5 +3,6 @@ from __future__ import annotations
 from dsutil.common import Singleton
 from dsutil.files import FileManager
 from dsutil.log import LocalLogger, TimeAwareLogger
+from dsutil.media import MediaManager
 from dsutil.text import Text
 from dsutil.tools import TZ, configure_traceback
