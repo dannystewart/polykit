02b069091f4bcce783b3d81818a45c13c3eb3be4
Danny Stewart <danny@stewart.cc>
2025-02-20 22:03:57 -0500

    FileManager (feature): Add macOS-specific methods to get and set file timestamps

--- src/dsutil/files/file_manager.py
+++ src/dsutil/files/file_manager.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import hashlib
 import shutil
+import subprocess
 import sys
 from typing import TYPE_CHECKING, Any
 
@@ -276,6 +277,38 @@ class FileManager:
                             "Error: Failed to permanently delete %s : %s", file_path.name, err
                         )
         return False
+
+    @staticmethod
+    def get_timestamps(file: Path) -> tuple[str, str]:
+        """Get file creation and modification timestamps. macOS only, as it relies on GetFileInfo.
+
+        Returns:
+            ctime: The creation timestamp.
+            mtime: The modification timestamp.
+        """
+        ctime = subprocess.check_output(["GetFileInfo", "-d", str(file)]).decode().strip()
+        mtime = subprocess.check_output(["GetFileInfo", "-m", str(file)]).decode().strip()
+        return ctime, mtime
+
+    @staticmethod
+    def set_timestamps(file: Path, ctime: str | None = None, mtime: str | None = None) -> None:
+        """Set file creation and/or modification timestamps. macOS only, as it relies on SetFile.
+
+        Args:
+            file: The file to set the timestamps on.
+            ctime: The creation timestamp to set. If None, creation time won't be set.
+            mtime: The modification timestamp to set. If None, modification time won't be set.
+
+        Raises:
+            ValueError: If both ctime and mtime are None.
+        """
+        if ctime is None and mtime is None:
+            msg = "At least one of ctime or mtime must be set."
+            raise ValueError(msg)
+        if ctime:
+            subprocess.run(["SetFile", "-d", ctime, str(file)], check=False)
+        if mtime:
+            subprocess.run(["SetFile", "-m", mtime, str(file)], check=False)
 
     @staticmethod
     def compare_mtime(file1: Path, file2: Path) -> float:
