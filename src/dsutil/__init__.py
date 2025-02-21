3ef45ffba5983f7699b94b903c21e7b9386c6567
Danny Stewart <danny@stewart.cc>
2025-02-20 19:21:36 -0500

    FileManager (feature): Add new FileManager class for file operations

--- src/dsutil/__init__.py
+++ src/dsutil/__init__.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from dsutil.common import Singleton
+from dsutil.files import FileManager
 from dsutil.log import LocalLogger, TimeAwareLogger
 from dsutil.text import Text
 from dsutil.tools import TZ, configure_traceback
