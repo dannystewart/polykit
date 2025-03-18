ddf57934c5eb390b2f44de145810738421051dd2
Danny Stewart <danny@stewart.cc>
2025-03-18 13:48:29 -0400

    shell (enhancement): Import animation functions for shell use

--- src/dsbase/shell/__init__.py
+++ src/dsbase/shell/__init__.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from .animation import conditional_animation, show_walking_animation, walking_animation
 from .progress import conversion_list_context, halo_progress, with_spinner
 from .shell import (
     acquire_sudo,
