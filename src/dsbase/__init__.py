b05830917ab36e49438ec8197c370fc8fd25dc18
Danny Stewart <danny@stewart.cc>
2025-03-22 19:43:15 -0400

    dsbase (feature): Import FileManager and MediaManager at package root

--- src/dsbase/__init__.py
+++ src/dsbase/__init__.py
@@ -24,5 +24,7 @@ See the individual module documentation for more detailed API information.
 from __future__ import annotations
 
 from dsbase.env import EnvManager
+from dsbase.files import FileManager
 from dsbase.log import LocalLogger
+from dsbase.media import MediaManager
 from dsbase.paths import PathKeeper
