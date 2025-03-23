33f5868dee3661dc857f1f49eaaf9f237ac40df7
Danny Stewart <danny@stewart.cc>
2025-03-23 16:24:39 -0400

    Text (fix): Import Text class methods for color and print_colored for backward compatibility

--- src/dsbase/text/__init__.py
+++ src/dsbase/text/__init__.py
@@ -1,5 +1,9 @@
 from __future__ import annotations
 
-from .color import color, info, print_colored, progress
+from .color import info, progress
 from .text import Text
 from .types import ColorAttrs, ColorName
+
+# NOTE: Consider removing these for 0.4.0
+color = Text.color
+print_colored = Text.print_colored
