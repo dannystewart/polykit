8ae56b4f08794dadfe600d56c6f5fb902a1927fa
Danny Stewart <danny@stewart.cc>
2025-03-30 21:13:44 -0400

    macos (refactor): Remove deprecated macOS functions and utilities, migrate notification functionality

--- src/dsbase/files/__init__.py
+++ src/dsbase/files/__init__.py
@@ -1,7 +1,4 @@
 from __future__ import annotations
 
-# NOTE: Remove this import for 0.4.0
-import dsbase.mac as macos
-
 from .compare import compare_files_by_mtime, find_duplicate_files_by_hash, sha256_checksum
 from .file_manager import FileManager
--- src/dsbase/mac/__init__.py
+++
@@ -1,3 +0,0 @@
-from __future__ import annotations
-
-from .macos import get_timestamps, set_timestamps, show_notification
