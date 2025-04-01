a8ae718e8036e71573e8cf6a27024e1c19cbc343
Danny Stewart <danny@stewart.cc>
2025-04-01 00:52:01 -0400

    __init__.py (docs): Update module docstring to match README

--- src/dsbase/__init__.py
+++ src/dsbase/__init__.py
@@ -1,21 +1,18 @@
-"""This is a delightful Python utility library that brings power and personality to your toolkit.
+"""A delightful Python utility library to bring power and personality to your toolkit.
 
-It contains various helpers refined through years of practical development, including an elegant
-logger, an environment variable manager, a path helper, database interfaces, file and media
-processing, and various other helpers that make common tasks a little easier or more joyful.
-Developed for personal use, but always to high standards of quality and flexibility.
+DSBase contains various tools and utilities refined through years of practical development, including a path helper, database interfaces, file and media processing, and various other helpers that make common tasks a little easier or more joyful. Developed for personal use, but always to high standards of quality and flexibility.
 
-**Note:** This library is constantly evolving, so for stability you may want to pin specific
-versions for now. I try to be careful about breaking changes but development is still very active.
+## Installation
+
+```bash
+pip install dsbase
+```
 
 ## Features
 
 Some of the features include:
 
-- `LocalLogger` for elegant and sophisticated logging that you'll love
-- `EnvManager` for clear setup and access to environment variables
 - `PathKeeper` for convenient cross-platform access to common paths
-- Thread-safe `Singleton` metaclass for use in any project
 - Drop-in `argparse` replacement with easier formatting
 - Simple helper for comparing files and showing diffs
 - Database helper interfaces for MySQL and SQLite
@@ -25,21 +22,8 @@ Some of the features include:
 - Simple progress indicators and helpers for common shell tasks
 - Loading animations that are both simple and charming
 - Comprehensive collection of text manipulation tools
-- Various time parsers and utilities, including a time-aware logger
-
-## Installation
-
-To install the library, simply run:
-
-```bash
-pip install dsbase
-```
-
-## Personal Scripts
-
-If you want to peruse my expansive collection of personal scripts, please head over to the
-[**dsbin**](https://github.com/dannystewart/dsbin/) library.
-"""  # noqa: D415
+- Various time parsers and utilities
+"""  # noqa: D415, W505
 
 from __future__ import annotations
 
